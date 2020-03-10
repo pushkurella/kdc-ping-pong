9c5bef681879af4445432cee19629a420ae4e64f
Pushpak Kurella <pushpak_kurella@cooperators.ca>
2020-03-09 22:41:26 -0400

    added html file

---
+++ precache-manifest.acc76145c18c650292f3b6f607529f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae1003385b8a51735de8bc24925503a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea7bcad64f827a36c687",
+    "url": "/static/css/main.cd0bca51.chunk.css"
+  },
+  {
+    "revision": "f18b5cad235afcfcada3",
+    "url": "/static/js/2.4b77e6e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4b77e6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea7bcad64f827a36c687",
+    "url": "/static/js/main.aaa4edef.chunk.js"
+  },
+  {
+    "revision": "48c53a525878510b8516",
+    "url": "/static/js/runtime-main.69161dfd.js"
+  }
+]);
